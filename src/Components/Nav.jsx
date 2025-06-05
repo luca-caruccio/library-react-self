ea49099ca465f93ee414b553aeb4ec09eb410e75
Luca <lfcaruccio@gmail.com>
2025-06-04 21:08:49 -0700

    Finally Corrected Nav error

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import LibraryLogo from '../assets/Library.svg';
 
 const Nav = () => {
     return (
         <nav>
             <div className="nav_container">
                 <a href="/">
-                <img src="" alt="" className="logo" />
+                <img src="{LibraryLogo}" alt="" className="logo" />
                 </a>
                 <ul className="nav__links">
                     <li className="nav__list">
@@ -21,7 +23,36 @@ const Nav = () => {
                     <button className="btn__menu">
                         <FontAwesomeIcon icon="bars" />
                     </button>
+                    <li className="nav__icon">
+                        <a href="/cart" className='nav__link'>
+                        <FontAwesomeIcon icon="shopping-cart"/>
+                        </a>
+                        <span className='cart__length'>3</span>
+                    </li>
                  </ul>
+                 <div className="manu__backdrop">
+                    <button className="btn__menu btn__menu--close">
+                        <FontAwesomeIcon icon='times'/>
+                    </button>
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <a href="/" className='menu__link'>
+                            Home </a>
+                        </li>
+                        </ul>
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <a href="/books" className='menu__link'>
+                            Books </a>
+                        </li>
+                        </ul>
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <a href="/cart" className='menu__link'>
+                            Cart </a>
+                        </li>
+                    </ul>
+                 </div>
             </div>
         </nav>
     );
