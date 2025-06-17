fa3f138c13f7d570aa1511338c13ac5b925fc3a7
Luca <lfcaruccio@gmail.com>
2025-06-16 21:22:38 -0700

    Highlights Completed, dynamic features implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+
+import './App.css';
+import Highlights from './Components/Highlights';
+import Landing from './Components/Landing';
 import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
      <Nav />
+     <Landing />
+     <Highlights />
     </div>
   );
 }
---
+++ src/Components/Highlights.jsx
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import Highlight from "./ui/Highlight";
+
+const Highlights = () => {
+  return (
+    <section id='highlights'>
+        <div className="container">
+            <div className="row">
+                <h2 className='section__title'>
+                    Why Choose <span className='purple'>Library</span>
+                </h2>
+                <div className="highlight__wrapper">
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon="bolt"/>} 
+                        title="Easy & Quick" 
+                        para="Get instant access to your online purchases!"
+                        />
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon="book-open"/>} 
+                        title="12,000+ books Available Instantly" 
+                        para="Books from all your favorite Categories Available"
+                        />
+                    <Highlight 
+                        icon={<FontAwesomeIcon icon="tags"/>} 
+                        title="Budget Friendly" 
+                        para="Find Popular and Trending Titles for as little as $9.99!"
+                        />
+                    
+                  
+                </div>
+            </div>
+        </div>
+    </section>
+  );
+};
+
+
+export default Highlights;
