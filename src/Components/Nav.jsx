e91803a1725d47e6225fa6b64d23c89a2e62efab
Luca <lfcaruccio@gmail.com>
2025-08-10 19:59:23 -0700

    Nav Issue Identified

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import LibraryLogo from '../assets/Library.svg';
+import LibraryLogo from "../assets/Library.svg";
 
 const Nav = () => {
+    function openMenu() {
+        document.body.classList += "menu--open";
+    }
+
+    function closeMenu() {
+        document.body.classList.remove("menu--open")
+    }
     return (
         <nav>
-            <div className="nav_container">
+            <div className="nav__container">
                 <a href="/">
-                <img src="{LibraryLogo}" alt="" className="logo" />
+                <img src={ LibraryLogo } alt="" className="logo" />
                 </a>
                 <ul className="nav__links">
                     <li className="nav__list">
-                        <a href="/" className="nav_link">
+                        <a href="/" className="nav__link">
                         Home
                         </a>
                     </li>
                     <li className="nav__list">
-                        <a href="/" className="nav_link">
+                        <a href="/" className="nav__link">
                         Books
                         </a>
                     </li>
-                    <button className="btn__menu">
+                    <button className="btn__menu" onClick={openMenu}>
                         <FontAwesomeIcon icon="bars" />
                     </button>
                     <li className="nav__icon">
                         <a href="/cart" className='nav__link'>
                         <FontAwesomeIcon icon="shopping-cart"/>
                         </a>
-                        <span className='cart__length'>3</span>
+                        <span className='cart__length'>2</span>
                     </li>
                  </ul>
                  <div className="manu__backdrop">
-                    <button className="btn__menu btn__menu--close">
+                    <button className="btn__menu btn__menu--close" onClick={closeMenu}>
                         <FontAwesomeIcon icon='times'/>
                     </button>
                     <ul className="menu__links">
