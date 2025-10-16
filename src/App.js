a8ad568c404e659735d23465b8aa1749e099de41
Luca <lfcaruccio@gmail.com>
2025-10-15 22:10:18 -0700

    Started 2.13

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
      <Nav />
      <Route path="/" exact component={Home}/>
      <Route path="/books" render={() => <Books books={books} />} />
+     <Route path="/books/1" render={() => <BookInfo books={books}/>}/>
      <Footer />
     </div>
     </Router>
