a123f01eb0da313c3dd687eaced1375f165d6df1
Luca <lfcaruccio@gmail.com>
2025-10-21 17:22:56 -0700

    BookInfo Error Fixed 2.13-1:42

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function BookInfo() {
+const BookInfo = ({ books }) => {
   return (
-    <div>
-      
-    </div>
+   <div id="books__body">
+    <main id="books__main">
+        <div className="books__container">
+            <div className="row">
+                <div className="book__selected--top"></div>
+            </div>
+        </div>
+    </main>
+   </div>
   )
 }
+
+export default BookInfo;
