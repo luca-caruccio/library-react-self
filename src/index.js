438d2ead052aff5e220c4d0cfbc8a06f13041220
Luca <lfcaruccio@gmail.com>
2025-11-11 23:10:48 -0800

    2.14 - 0/0

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt)
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Link } from "react-router-dom";
+import Rating from '../Components/ui/Rating';
+import Price from '../Components/ui/Price';
 
 const BookInfo = ({ books }) => {
   return (
@@ -12,6 +14,20 @@ const BookInfo = ({ books }) => {
                     <Link to="/books" className="book__link">
                     <FontAwesomeIcon icon="arrow-left" />
                     </Link>
+                    <Link to="/books" className="book__link"><h2 className='book__selected--title--top'>Books</h2>
+                    </Link>
+                </div>
+                <div className="book__selected">
+                    <figure className="book__selected--figure">
+                        <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
+                    </figure>
+                    <div className="book__selected--description">
+                       <h2 className="book__selected--title">Crack the Coding Interview</h2>
+                       <Rating rating="4.5" />
+                       <div className="book__selected--price">
+                        <Price originalPrice={ 50 } salePrice={ null }/>
+                       </div>
+                    </div>
                 </div>
             </div>
         </div>
