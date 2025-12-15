3e35f56c45f9d07420c40597b1afdf3537b69c37
Luca <lfcaruccio@gmail.com>
2025-12-14 16:53:08 -0800

    2.15 - Under Review

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import Rating from '../Components/ui/Rating';
 import Price from '../Components/ui/Price';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import Book from '../Components/ui/book';
 
 const BookInfo = ({ books }) => {
+    const { id } =  useParams();
+    const book = books.find(book => +book.id === +id);
   return (
    <div id="books__body">
     <main id="books__main">
@@ -19,15 +23,54 @@ const BookInfo = ({ books }) => {
                 </div>
                 <div className="book__selected">
                     <figure className="book__selected--figure">
-                        <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--ig" />
+                        <img src={book.url} />
                     </figure>
                     <div className="book__selected--description">
-                       <h2 className="book__selected--title">Crack the Coding Interview</h2>
-                       <Rating rating="4.5" />
+                       <h2 className="book__selected--title">{book.title}</h2>
+                       <Rating rating={book.rating} />
                        <div className="book__selected--price">
-                        <Price originalPrice={ 50 } salePrice={ null }/>
+                        <Price originalPrice={ book.originalPrice } salePrice={ book.salePrice }/>
                        </div>
+                       <div className="book__summary">
+                        <div className="book__summary--title">
+                            Summary
+                        </div>
+                        <h3 className="book_summary--para">
+                            Lorem ipsum dolor sit amet consectetur apisicing elit. 
+                            Delectus omnis a, laudantium sed sit, neque rem maiores 
+                            vero aspernatur cumque debitis quam vitae deleniti, eum 
+                            labore. Neque veritatis ab cumque eum ratione esse, ex 
+                            alias, eius excepturi, temporibus maxime autem.
+                        </h3>
+                        <h3 className="book_summary--para">
+                            Lorem ipsum dolor sit amet consectetur apisicing elit. 
+                            Delectus omnis a, laudantium sed sit, neque rem maiores 
+                            vero aspernatur cumque debitis quam vitae deleniti, eum 
+                            labore. Neque veritatis ab cumque eum ratione esse, ex 
+                            alias, eius excepturi, temporibus maxime autem.
+                        </h3>
+                       </div>
+                       <button className='btn'>
+                        Add to Cart
+                       </button>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="books__container">
+            <div className="row">
+                <div className="book__selected--top">
+                    <h2 className="book__selected--title--top">
+                        Recommended Books
+                    </h2>
+                </div>
+                <div className="books">
+                {
+                    books.filter(book => book.rating === 5 && +book.id !== +id)
+                    .slice(0,4)
+                    .map(book => <Book book={book} key={book.id} />)
+                }
                 </div>
             </div>
         </div>
