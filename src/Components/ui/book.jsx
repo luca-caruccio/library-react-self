bfdfe057b8f3d5b175bbfc379e65d01baea61c46
Luca Caruccio <lfcaruccio@gmail.com>
2026-06-02 22:47:28 -0700

    Finished 6.loadingstate - pending review for functionality

--- src/Components/ui/book.jsx
+++ src/Components/ui/book.jsx
@@ -1,15 +1,36 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link } from "react-router-dom"
 import Rating from "./Rating"
 import Price from './Price';
 
 const Book = ({ book }) => {
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true)
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onLoad = () => {
+      setTimeout(() => {
+        if (mountedRef.current) {
+      setImg(image);
+        }
+      }, 300);
+      
+    };
+    return() => {
+      mountedRef.current = false;
+    }
+  })
+  
   return (
    <div className="book">
-                     <Link to={'/books/{book.id}'}>
+    {
+      img ? (<> <Link to={'/books/${book.id}'}>
                        <figure className="book__img--wrapper">
-                         <img src={book.url} alt="" />
+                         <img 
+                         src={img.src} alt="" className= 'book__img' />
                        </figure>
                      </Link>
                      <div className="book__title">
@@ -18,7 +39,16 @@ const Book = ({ book }) => {
                        </Link>
                      </div>
                      <Rating rating={ book.rating }/>
-                     <Price salePrice= {book.salePrice} originalPrice={book.originalPrice}/>
+                     <Price salePrice= {book.salePrice} originalPrice={book.originalPrice}/> 
+                     </> ) :  <>  
+                     <div className="book__img--skeletom"></div>
+    <div className="skeleton book__title--skeleton"></div>
+    <div className="skeleton book__rating--skeleton"></div>
+    <div className="skeleton book__price--skeleton"></div> 
+    </>
+    }
+
+                    
                    </div>
   )
 }
